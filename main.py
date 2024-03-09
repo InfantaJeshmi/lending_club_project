7e80f9aafccf457cefde52fa865969db6d32718a
unknown <jinfanta@gmail.com>
2024-03-09 20:51:02 +0100

    making few changes in the lib and main file of lendingclubprojectDemo

---
+++ main.py
@@ -0,0 +1,3 @@
+#This is main file. this contains code which binds all the files in the project folder togather.
+
+# i am making few changes in this files. lets see how it reflects after git commit and push in github
