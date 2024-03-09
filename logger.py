1a7c10e2c0c2779302d16ea3fc20e58513081d2b
unknown <jinfanta@gmail.com>
2024-03-09 21:05:18 +0100

    making changes in main branch files

---
+++ logger.py
@@ -0,0 +1,2 @@
+# This is logger file. it contains the log messages configurations. spark uses log4j for logging.
+
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
 #This is main file. this contains code which binds all the files in the project folder togather.
 
 # i am making few changes in this files. lets see how it reflects after git commit and push in github
+
+# this code is for git main branch
