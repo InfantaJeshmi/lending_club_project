ed17f51df795c47beb5605285d726ec02b7b4a90
unknown <jinfanta@gmail.com>
2024-03-09 21:31:56 +0100

    commiting changes

--- logger.py
+++ logger.py
@@ -1,9 +1,9 @@
-<<<<<<< HEAD
+
 # This is logger file. it contains the log messages configurations. spark uses log4j for logging.
 
 # main branch 
-=======
+
 # this is logger file. for configuration of log messages. spark uses log4j for logging.
 
 # this data is for fearture1 git branch
->>>>>>> 0b61b2d006c0cf81cc2b9fc523f6d4121d69912a
+
--- main.py
+++ main.py
@@ -2,10 +2,9 @@
 
 # i am making few changes in this files. lets see how it reflects after git commit and push in github
 
-<<<<<<< HEAD
 # this code is for git main branch
-=======
+
 # i am making changes in the main file on feature1 branch.....
 
 ## feature1 branch...
->>>>>>> 0b61b2d006c0cf81cc2b9fc523f6d4121d69912a
+
